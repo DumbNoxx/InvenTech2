b36b10bd38b3b074311afcdb034944db13a938ed
DumbNox <kevinmarcano0921@gmail.com>
2024-09-04 15:38:44 -0400

    arreglando el masterpanel

--- form/master/form_master.py
+++ form/master/form_master.py
@@ -18,15 +18,18 @@ class MasterPanel:
 		self.ventana.config(bg='#09ab53')
 		self.ventana.resizable(width=0,height=0)
 
-		logo = util.leer_image(Logo,(200,200))
+		logo = util.leer_image(Logo,(350,350))
 
-		label1 = tk.Label(self.ventana,image=logo,bg='#fcfcfc')
+		label1 = tk.Label(self.ventana,image=logo,bg='#1abc9c')
 		label1.place(x=0,y=0,relwidth=1,relheight=1)
 
 		def calcu():
 			CalculadoraCreate().mainloop()
 
-		boton = tk.Button(self.ventana,text='probando',command=calcu)
-		boton.pack()
+		boton = tk.Button(self.ventana,text='Calculadora',bg='#a3e4d7',fg='black',command=calcu)
+		boton.place(x=620,y=170,relwidth=0.09,relheight=0.07)
+
+		boton1 = tk.Button(self.ventana,text='Agregar Producto',bg='#a3e4d7',fg='black')
+		boton1.place(x=450,y=200,relwidth=0.09,relheight=0.07)
 
 		self.ventana.mainloop()
